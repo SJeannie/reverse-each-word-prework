51597cda4416ef080eeacdaae040bd2035055a59
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-15 07:49:48 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(sentence)
+  
+  reversed_words = []
+  
+  words = sentence.split(/ /) 
+  words.collect do |x|
+    reversed_words.push(x.reverse)
+  end 
+  
+  reversed_words.join(" ")
+
+end
+
