e19d241e081976cf0b29ab48f36c4feadc4db410
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-15 07:34:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++
@@ -1,9 +0,0 @@
-def reverse_each_word(sentence)
-  
-  sentence.to_a
-  
-  it 'uses collect' do
-    expect_any_instance_of(Array).to receive(:collect).and_call_original
-    reverse_each_word("Verifying that collect is being called.")
-  end
-end
